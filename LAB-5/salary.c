56391029f4484feaab524faa764a5e61268b1925
manalli <143473743+manalli@users.noreply.github.com>
2023-08-29 09:16:08 +0530

    Update salary.c

--- LAB-5/salary.c
+++ LAB-5/salary.c
@@ -3,13 +3,13 @@ void main(){
 	float a,b;
 	printf("Enter the Basic Salary:");
 	scanf("%f",&a);
-	if(a>= 10000){
-		printf("Gross Salary =%f",b=(a*0.2)+(a*0.8));
+	if(a>= 10000&& a<20000){
+		printf("Gross Salary =%f", b=a+(a*0.2)+(a*0.8));
 	}
-	if(a>= 20000){
-		printf("Gross Salary =%f",b=(a*0.2)+(a*0.8));
+	if(a>= 20000 && a<30000){
+		printf("Gross Salary =%f", b=a+(a*0.25)+(a*0.9));
 	}
 	if(a>= 30000){
-		printf("Gross Salary =%f",b=(a*0.2)+(a*0.8));
+		printf("Gross Salary=%f", b=a+(a*0.3)+(a*0.95));
 	}
 }
